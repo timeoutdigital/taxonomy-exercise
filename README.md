796d494b29db62118bff4f20a44194bba1017a81
Kun Cai <kuncai@timeout.com>
2017-08-23 16:59:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Taxonomy Tree Exercise
+
+[Full exercise in PDF format](doc/exercise.pdf)
+
+![exercise](doc/exercise.png)
---
+++ src/main/scala/com/timeout/interview/Main.scala
@@ -0,0 +1,7 @@
+package com.timeout.interview
+
+object Main extends App {
+
+  println("Feel free to add your own code in here, good luck!")
+
+}
